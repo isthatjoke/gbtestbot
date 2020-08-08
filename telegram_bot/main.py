0b48dd0e304042db6cfeb81ba7c7d1dc5446064c
Timur Zverev <61158881+isthatjoke@users.noreply.github.com>
2020-08-08 21:16:40 +0300

    new_one

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,1 +1,13 @@
-if __name__ == "__main__"
+import requests
+
+bot_api = 'https://api.telegram.org/bot1337111137:AAELaRg_ixU9Wnx7FYmjXL2TkL1XCAssCUQ/'
+weather_api = '57e77fa67b7f1374ba14b7f5022bfff1'
+#https://openweathermap.org/current
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass

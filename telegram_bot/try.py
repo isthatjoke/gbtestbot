81a24b6a3a7585778d1328d30a9e72af4100a585
Timur Zverev <61158881+isthatjoke@users.noreply.github.com>
2020-08-27 22:40:42 +0300

    bot_stable_v1

---
+++ telegram_bot/try.py
@@ -0,0 +1,31 @@
+new = {
+    'message_id': 763,
+    'date': 1597689690,
+    'chat':
+        {
+            'id': 300793622,
+            'type': 'private',
+            'username': 'is_that_joke',
+            'first_name': 'Timur',
+            'last_name': 'Zverev'
+        },
+    'text': 'В процессе написания кода...',
+    'entities': [],
+    'caption_entities': [],
+    'photo': [],
+    'new_chat_members': [],
+    'new_chat_photo': [],
+    'delete_chat_photo': False,
+    'group_chat_created': False,
+    'supergroup_chat_created': False,
+    'channel_chat_created': False,
+    'from':
+        {
+            'id': 1337111137,
+            'first_name': 'is_that_joke_bot',
+            'is_bot': True,
+            'username': 'is_that_joke_bot'
+        }
+}
+print(1)
+
